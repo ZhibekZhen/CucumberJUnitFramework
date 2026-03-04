5178a6762c33da59630d9bb82bd36eb13b5d07b9
User <user@Users-MacBook-Pro.local>
2026-03-03 21:26:27 -0600

    add project

---
+++ src/test/java/utils/BaseUI.java
@@ -0,0 +1,52 @@
+package utils;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseUI {
+    public void waitAndClick(WebElement element){
+        waitUntilClickable(20, element);
+        element.click();
+    }
+
+    public void waitAndSendKeys(WebElement element, String keys){
+        waitUntilVisible(20, element);
+        element.sendKeys(keys);
+    }
+
+    public void jsClick(WebElement element){
+        waitUntilClickable(20, element);
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].click();", element);
+    }
+
+    public void jsSendKeys(WebElement element, String value){
+        waitUntilVisible(20, element);
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].value='';", element);
+        js.executeScript("arguments[0].value=arguments[1];", element, value);
+    }
+
+    public WebDriverWait explicitWait(int seconds){
+        return new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(seconds));
+    }
+
+    public void waitUntilClickable(int seconds, WebElement element){
+        new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(seconds))
+                .until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void waitUntilVisible(int seconds, WebElement element){
+        new  WebDriverWait(Driver.getDriver(), Duration.ofSeconds(seconds))
+                .until(ExpectedConditions.visibilityOf(element));
+    }
+
+
+
+}
+
+
